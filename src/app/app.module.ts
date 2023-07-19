841b1ed434f50a2e97b1443fe75bfa2e46d93cfb
Sagar Dighe <sagasoftech@gmail.com>
2023-07-18 22:01:28 -0500

    Remove LogOut componenet as it will be handled by Keycloak

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,6 @@ import { HeaderComponent } from './components/header/header.component';
 import { ContactComponent } from './components/contact/contact.component';
 import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { LogoutComponent } from './components/logout/logout.component';
 import { NoticesComponent } from './components/notices/notices.component';
 import { AccountComponent } from './components/account/account.component';
 import { BalanceComponent } from './components/balance/balance.component';
@@ -39,7 +38,6 @@ function initializeKeycloak(keycloak: KeycloakService) {
     ContactComponent,
     LoginComponent,
     DashboardComponent,
-    LogoutComponent,
     NoticesComponent,
     AccountComponent,
     BalanceComponent,
